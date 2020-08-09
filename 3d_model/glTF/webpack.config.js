a68e754dc481f9826eca6f68fea4d294951fc115
r21nomi <r21nomi@gmail.com>
2020-08-09 19:06:11 +0900

    Make production build

--- 3d_model/glTF/webpack.config.js
+++ 3d_model/glTF/webpack.config.js
@@ -1,9 +1,7 @@
 let path = require('path');
 
 module.exports = {
-    mode: 'development',
     entry: './src/index.js',
-    devtool: 'inline-source-map',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'public')
